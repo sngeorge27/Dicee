60c486a9a5f51f60874de8e03f69262357fcdd05
Sarah George <sngeorge27@gmail.com>
2019-07-12 11:57:27 -0400

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Sarah George on 7/12/19.
+//  Copyright © 2019 Sarah George. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
